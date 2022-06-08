002d44ed918b36c6a2ba041754cb9c1a0b73d87b
Uchkun Ashirov <uchkunashirov@Uchkuns-MacBook-Pro.local>
2022-06-08 06:23:43 +0500

    Dastlabki fayllar yaratildi

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+
+ReactDom.render(
+  <div>
+      <App  />
+  </div>,
+
+  document.getElementById('root')
+);
